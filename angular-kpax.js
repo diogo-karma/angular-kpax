a7b4ec15a73dcc89d8ce3ce6690ebf1053401927
Nechtan <dnechtan@gmail.com>
2014-04-29 18:30:59 -0300

    Several fixes and tridiretional communication

--- angular-kpax.js
+++ angular-kpax.js
@@ -100,20 +100,18 @@ angular.module('ngKpax', ['ngSocketIO'])
           Math.random().toString(36));
       };
 
-      var $emit = function $emit(method, key, params, callback, options) {
-        if (angular.isFunction(params)) {
-          if (angular.isObject(callback) && !options) {
-            options = callback;
-          }
-          callback = params
-        }
+      var $emit = function $emit(options, callback) {
         options = angular.extend({
-          cache: true
+          cache: true,
+          method: 'get',
+          url: '',
+          params: {},
+          data: {}
         }, options || {});
         var cached = false;
-        var hash = newHash(key);
+        var hash = newHash(options.method + ':' + options.url);
         if (options.cache) {
-          var cacheKey = JSON.stringify([method, key, params]);
+          var cacheKey = JSON.stringify([options.method, options.url, options.params]);
           if (cached = cache.get(cacheKey)) {
             callback(cached.data);
             return cached._hash;
@@ -122,28 +120,52 @@ angular.module('ngKpax', ['ngSocketIO'])
         _fn[hash] = callback;
         socket.emit('kpax', {
           _hash: hash,
-          _key: method + ':' + key,
+          _key: options.method + ':' + options.url,
           _cache: [options.cache, cacheKey],
-          params: params || {}
+          params: options.params
         });
         return hash;
       };
 
-      var $on = function $on(type, key, callback) {
-        _fn[type + ':' + key] = callback;
+      var $on = function $on(verb, key, callback) {
+        // Object jQuery-Ajax-Style
+        if (angular.isObject(verb)) {
+          var opt = verb;
+          if (opt.hasOwnProperty('type'))
+            verb = opt.type;
+          if (opt.hasOwnProperty('method'))
+            verb = opt.method;
+          if (opt.hasOwnProperty('url'))
+            key = opt.url
+          if (angular.isFunction(key)) {
+            callback = key;
+          } else {
+            if (opt.hasOwnProperty('success')) {
+              callback = opt.success;
+            }
+          }
+        }
+        console.log('set $on', verb + ':' + key);
+        _fn[verb + ':' + key] = callback || function (req, res) {
+          res.send({});
+        };
       };
 
       socket.on('kpax', function (data) {
+        console.log('on kpax', data);
         if (_fn.hasOwnProperty(data._key) && angular.isFunction(_fn[data._key])) {
           console.log('$on _key', data._key);
+          var _emit = function (ret) {
+            socket.emit('kpax', {
+              _hash: data._hash,
+              _key: data._key,
+              data: ret
+            });
+          };
           _fn[data._key].call(socket, data, {
-            send: function (params, data) {
-              socket.emit('kpax', {
-                _hash: data._hash,
-                _key: data._key,
-                data: data
-              });
-            }
+            send: _emit,
+            emit: _emit,
+            json: _emit
           });
           return true;
         }
@@ -163,14 +185,28 @@ angular.module('ngKpax', ['ngSocketIO'])
       });
 
       ['get', 'post', 'delete', 'del', 'put', 'head'].map(function (verb) {
-        verbs[verb] = $emit.bind(self, verb);
+        verbs[verb] = function(url, data, params, callback) {
+          if(angular.isFunction(params)) {
+            callback = params;
+          }
+          $emit({
+            url: url,
+            method: verb,
+            params: params,
+            data: data,
+            callback: callback
+          })
+        };
       });
 
       verbs.on = $on.bind(socket);
       verbs.emit = verbs.send = $emit.bind(socket);
       verbs.socket = socket;
       verbs.cache = cache;
-
+      verbs.identify = function kpaxIdentify(id) {
+        socket.emit('kpax:identify', id);
+        return verbs;
+      };
       return verbs;
 
 }]);
